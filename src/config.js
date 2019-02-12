d76511027dce53617192278dacb6db52bd65c6b3
Samuel Omole <samuelomole70@gmail.com>
2019-02-12 17:22:46 +0100

    moved over to the config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  appId: "59802d7162a931126d875da1dbe02597e323efa1", //Enter your App ID
-  apiKey: "1395138329277", //Enter your API KEY
+  appId: "1395138329277", //Enter your App ID
+  apiKey: "59802d7162a931126d875da1dbe02597e323efa1", //Enter your API KEY
   GUID: "supergroup", // Enter your group UID
 };
